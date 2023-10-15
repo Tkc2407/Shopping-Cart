b9bb0822051863cfd7cfcc42e17370a3722be1c3
Tkc2407 <tapaschaubey@gmail.com>
2023-10-16 03:09:14 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,84 @@
-import { Search } from '@mui/icons-material';
-import React from 'react'
-import styled from 'styled-components'
+import { Search } from "@mui/icons-material";
+import React from "react";
+import styled from "styled-components";
+import Badge from '@mui/material/Badge';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
-const Container=styled.div`
-    height:60px;
-    // background-color:red
-
+const Container = styled.div`
+//   height: 60px;
+  // background-color:red
 `;
-const Wrapper=styled.div`
-    padding:10px 20px;
-    display:flex;
-    justify-content:space-between;
-    align-items:center;
-`;
-
-
-const Left =styled.div`
-    flex:1;
-    display:flex;
-    align-items:center;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
-const Language=styled.span`
-    font-size:14px;
-    cursor:pointer;
-    
-`;
-const SearchContainer=styled.div`
-    border:0.5px solid lightgray;
-    display:flex;
-    align-items:center;
-    margin-left:25px;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
 `;
 
-const Input=styled.input`
-    border:none;
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
 `;
 
-const Centre =styled.div`
+const Input = styled.input`
+  border: none;
+`;
+
+const Centre = styled.div`
     flex:1
     text-align:center;
 `;
 
-const Logo=styled.h1`
-    font-weight:bold;
+const Logo = styled.h1`
+  font-weight: bold;
 `;
-const Right =styled.div`
-    flex:1
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
-
-
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+    margin-left: 25px;
+`;
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
         <Left>
-            <Language>EN</Language>
-            <SearchContainer>
-                <Input/>
-                <Search/>
-            </SearchContainer>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input style={{color:"gray", fontsize:16}}/>
+            <Search />
+          </SearchContainer>
         </Left>
         <Centre>
-            <Logo>TAPS</Logo>
+          <Logo>TAPS</Logo>
         </Centre>
-        <Right>right</Right>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon color="action" />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
