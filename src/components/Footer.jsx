eaeef9e75ae2802811d8c3dfe54ae28d06e8319b
Tkc2407 <tapaschaubey@gmail.com>
2023-12-27 21:02:40 +0530

    Addes Responsive Designs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,12 @@ import {
     Twitter,
   } from "@mui/icons-material";
   import styled from "styled-components";
-//   import { mobile } from "../responsive";
+  import { mobile } from "../responsive";
 
   
   const Container = styled.div`
     display: flex;
+    ${mobile({flexDirection:"column"})}
   `;
   
   const Left = styled.div`
@@ -48,6 +49,7 @@ import {
   const Center = styled.div`
     flex: 1;
     padding: 20px;
+    ${mobile({display:"none"})}
   `;
   
   const Title = styled.h3`
@@ -70,6 +72,7 @@ import {
   const Right = styled.div`
     flex: 1;
     padding: 20px;
+    ${mobile({backgroundColor:"#fff8f8"})}
   
   `;
   
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,19 @@ import React from "react";
 import styled from "styled-components";
 import Badge from '@mui/material/Badge';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import {mobile} from "../responsive"
 
 const Container = styled.div`
-//   height: 60px;
-  // background-color:red
+  height: 60px;
+  /* background-color:red */
+  ${mobile({height:"50px",width:"100%"})}
 `;
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  ${mobile({padding:"10px 0px"})}
 `;
 
 const Left = styled.div`
@@ -24,6 +27,7 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({display:"none"})}
 `;
 const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
@@ -34,6 +38,7 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({width:"50px"})}
 `;
 
 const Centre = styled.div`
@@ -43,12 +48,14 @@ const Centre = styled.div`
 
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({fontsize:"24px"})}
 `;
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({flex:2,justifyContent: "center"})}
 
 `;
 
@@ -56,6 +63,7 @@ const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
     margin-left: 25px;
+    ${mobile({fontSize:"12px",marginLeft:"10px"})}
 `;
 const Navbar = () => {
   return (
@@ -64,7 +72,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input style={{color:"gray", fontsize:16}}/>
+            <Input placeholder="Search" style={{color:"gray", fontsize:16}}/>
             <Search />
           </SearchContainer>
         </Left>
